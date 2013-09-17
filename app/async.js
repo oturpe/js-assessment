c7a3d594050265d25535504d3b904be0e1a00ffa
Otto Urpelainen <oturpe@iki.fi>
2013-09-17 18:33:03 +0200

    Too simple solution to first async problem

--- app/async.js
+++ app/async.js
@@ -3,7 +3,9 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
-
+      return {
+        then: function (onSuccess) { return onSuccess(value); }
+      }
     },
 
     manipulateRemoteData : function(url) {
