147cad0c2b5ed25721d3c274a01443523af373aa
Otto Urpelainen <oturpe@iki.fi>
2013-09-17 18:56:16 +0200

    Proper asynchronous solution for async

--- app/async.js
+++ app/async.js
@@ -3,8 +3,12 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
+      var callback;
+      
+      setTimeout(function () { return callback(value); }, 100);
+      
       return {
-        then: function (onSuccess) { return onSuccess(value); }
+        then: function (onSuccess) { callback = onSuccess; }
       }
     },
 
